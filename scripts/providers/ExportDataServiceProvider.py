0d566db6de5f53be477e9c1a8563528cc5ffc0a1
Anochima <chimaobinice@gmail.com>
2022-10-06 23:01:25 +0100

    deleted models.py

--- scripts/providers/ExportDataServiceProvider.py
+++ scripts/providers/ExportDataServiceProvider.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+import os
+import boto3
+import pandas as pd
 from typing import List
+from botocore import UNSIGNED
+from botocore.client import Config
 from handlers.env_handler import env
 from sqlalchemy import create_engine
+from handlers.log_handler import LogHandler
 from handlers.service_handler import Service
+from handlers.db_connect_handler import DatabaseConn
 
-from models import export_to_db, export_to_warehouse
+logger = LogHandler("logs/export.log")
 
 
 class ExportDataServiceProvider(Service):
@@ -46,13 +53,78 @@ class ExportDataServiceProvider(Service):
 
     def execute_service(self):
         if self.export_to == "DB":
-            export_to_db(files=self.service_list, schema=self.default_schema)
+            self.__export_to_db(files=self.service_list, schema=self.default_schema)
 
         elif self.export_to == "WAREHOUSE":
-            export_to_warehouse(table_names=self.service_list)
+            self.__export_to_warehouse(table_names=self.service_list)
         else:
             raise TypeError(
                 f"Nothing happened, please check {__file__} execute_service() method"
             )
 
         print("Upload Completed! \n")
+
+    def __export_to_warehouse(
+        self,
+        table_names: List = [
+            "agg_public_holiday",
+            "agg_shipments",
+            "best_performing_product",
+        ],
+        schema=env("SERVER", "DB_ANALYTICS_SCHEMA"),
+        bucket=env("SERVER", "S3_WAREHOUSE_BUCKET_NAME"),
+    ):
+        """Upload a file to an Warehouse bucket"""
+
+        conn = DatabaseConn(connector="alchemy")
+        engine = conn.connect()
+        for table in table_names:
+            query = pd.read_sql_query(f"""SELECT * FROM {schema}.{table}""", con=engine)
+            df = pd.DataFrame(query)
+            df.to_csv(f"../d2b/data/transformed/{table}.csv", index=False)
+
+        upload_path = "../d2b/data/transformed"
+        for file in os.listdir("../d2b/data/transformed"):
+            object_name = os.path.basename(file)
+            file = "/".join([upload_path, object_name])
+
+            # Upload the file
+            s3_client = boto3.client("s3", config=Config(signature_version=UNSIGNED))
+            try:
+                print(f"uploading... {file}")
+                response = s3_client.upload_file(file, bucket, object_name)
+            except Exception as e:
+                logger.logger.error(e)
+                print(e)
+        print("------" * 20)
+
+    def __export_to_db(self, files: List, schema=env("SERVER", "DB_STAGING_SCHEMA")):
+        """Exports files to database
+
+        Args:
+            files (List): A list of filepaths
+            schema (_type_, optional): _description_. Defaults to env("SERVER", "DB_STAGING_SCHEMA").
+        """
+        try:
+
+            conn = DatabaseConn(connector="alchemy")
+            engine = conn.connect()
+            for file in files:
+                # Get the table name
+                table_name = os.path.split(file)[-1].split(".")[0]
+
+                data = pd.read_csv(file, delimiter=",", index_col=None).reset_index(
+                    drop=True
+                )
+                data.to_sql(
+                    name=table_name,
+                    con=engine,
+                    schema=schema,
+                    if_exists="replace",
+                    method="multi",
+                )
+                print(f"{table_name} records seeded successfuly")
+        except Exception as e:
+            print(e)
+            logger.logger.error(e)
+        print("------" * 20)
--- scripts/providers/ImportDataServiceProvider.py
+++ scripts/providers/ImportDataServiceProvider.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-
+import boto3
 from typing import List
+from botocore import UNSIGNED
+from botocore.client import Config
 from handlers.env_handler import env
 from handlers.log_handler import LogHandler
 from handlers.service_handler import Service
-from models import import_from_warehouse, import_from_db
+from handlers.db_connect_handler import DatabaseConn
 
-handler = LogHandler("logs/import.log")
+logger = LogHandler("logs/import.log")
 
 
 class ImportDataServiceProvider(Service):
@@ -39,13 +41,49 @@ class ImportDataServiceProvider(Service):
 
     def execute_service(self):
 
-        if self.import_from == "DB":
-            import_from_db(files=self.service_list)
+        if self.import_from.lower() == "db":
+            self.__import_from_db(files=self.service_list)
 
-        elif self.import_from == "WAREHOUSE":
-            import_from_warehouse(object_names=self.service_list)
+        elif self.import_from.lower() == "warehouse":
+            self.__import_from_warehouse(object_names=self.service_list)
         else:
             print(f"Nothing happened, please check {__file__} execute_service() method")
             return None
 
         print("Import completed! \n")
+
+    def __import_from_warehouse(
+        self,
+        object_names: List,
+        bucket_name=env("SERVER", "S3_WAREHOUSE_BUCKET_NAME"),
+        prefix="orders_data",
+        path="../d2b/data/raw",
+    ):
+        """
+        Extracts raw data from specified cloud (AWS) s3: bucket
+
+        Args:
+        object_names: a list of warehouse objects
+        bucket_name: Name of the warehouse bucket
+        prefix: warehouse object name prefix
+        path: path where downloaded file will be stored
+        """
+
+        try:
+            conn = DatabaseConn(connector="alchemy")
+            engine = conn.connect()
+            for object_name in object_names:
+                save_path = "/".join([path, object_name])
+                object_path = "/".join([prefix, object_name])
+                s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
+                # response = s3.list_objects(Bucket=bucket_name, Prefix=prefix)
+                s3.download_file(bucket_name, object_path, save_path)
+                print(f"{object_name} imported successfully")
+        except Exception as e:
+            print(e)
+            print("An error occcured, check", logger.file_handler.baseFilename)
+            logger.logger.debug(e)
+        print("------" * 20)
+
+    def __import_from_db(self, files: List):
+        return "Feature coming!"
