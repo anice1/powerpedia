6adda208afe335697e0bb754e0e7082665ccf99c
Anochima <chimaobinice@gmail.com>
2022-10-06 22:34:28 +0100

    code review completed

--- scripts/handlers/__init__.py
+++
@@ -1,4 +0,0 @@
-# from handlers.db_connect_handler import DatabaseConn
-# from handlers.env_handler import env
-# from handlers.log_handler import LogHandler
-# from handlers.service_handler import Service
